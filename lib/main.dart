1b4ea24e7ceeb87a3a466c55a2c8cc52115e7fd3
cheri-nonegolab <cheri@nonegolab.com>
2022-11-01 20:36:07 +0900

    feat ::: 블루투스 bottomSheet 제작

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:juvis_prac/widget/bluetooth_bottom_sheet.dart';
 import 'measure.dart';
 import 'exercise.dart';
 
@@ -35,24 +36,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool toggleIcon = false;
+
+  toggleIconState(bool value) {
+    setState(() {
+      toggleIcon = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+            isScrollControlled: true,
+            builder: (context) {
+              return BluetoothBottomSheet();
+            },
+          );
+        },
+        child: Icon(
+          Icons.bluetooth,
+          color: Colors.black,
+        ),
+      ),
       body: Container(
-          height: 500,
-          width: double.infinity,
-          color: Colors.white,
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(bottom: 50, top: 150),
-              child: Text("여기가 운동 메인 페이지", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        height: 500,
+        width: double.infinity,
+        color: Colors.white,
+        child: Column(children: [
+          const Padding(
+            padding: EdgeInsets.only(bottom: 50, top: 150),
+            child: Text("여기가 운동 메인 페이지",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.only(
+                  left: 16, right: 16, top: 10, bottom: 10),
+              textStyle: const TextStyle(fontSize: 16),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // <-- Radius
+              ),
             ),
-            ElevatedButton(
+            child: const Text("측정 페이지 이동"),
+            onPressed: () {
+              Navigator.pushNamed(context, '/measure');
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
+                padding: const EdgeInsets.only(
+                    left: 16, right: 16, top: 10, bottom: 10),
                 textStyle: const TextStyle(fontSize: 16),
                 foregroundColor: Colors.white,
                 backgroundColor: Colors.black,
@@ -60,31 +104,13 @@ class _MyHomePageState extends State<MyHomePage> {
                   borderRadius: BorderRadius.circular(10), // <-- Radius
                 ),
               ),
-              child: const Text("측정 페이지 이동"),
-              onPressed:() {
-                Navigator.pushNamed(context, '/measure');
+              child: const Text("운동 페이지 이동"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/exercise');
               },
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
-                  textStyle: const TextStyle(fontSize: 16),
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // <-- Radius
-                  ),
-                ),
-                child: const Text("운동 페이지 이동"),
-                onPressed:() {
-                  Navigator.pushNamed(context, '/exercise');
-                },
-              ),
-            ),
-          ]
-        ),
+          ),
+        ]),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
---
+++ lib/widget/bluetooth_bottom_sheet.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:juvis_prac/bluetooth/puck.dart';
+
+class BluetoothBottomSheet extends StatefulWidget {
+  BluetoothBottomSheet({super.key});
+
+  @override
+  _BluetoothBottomSheetState createState() => _BluetoothBottomSheetState();
+}
+
+class _BluetoothBottomSheetState extends State<BluetoothBottomSheet> {
+  bool isFinding = false;
+  Puck puck = Puck();
+
+  void closeModal() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.9,
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        children: [
+          ListTile(
+              trailing: TextButton(
+                child: Text(
+                  '문의하기',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {},
+              ),
+              leading: GestureDetector(
+                onTap: closeModal,
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.black,
+                ),
+              )),
+          Expanded(
+              child: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                '내 기기 찾기',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'LED 점멸 여부로 내 기기 확인 후 등록을 진행하세요',
+                style: TextStyle(fontSize: 10, color: Colors.grey[600]),
+              ),
+              isFinding == true
+                  ? OutlinedButton(
+                      onPressed: () {
+                        setState(() {
+                          isFinding = false;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(9999, 40),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.pause,
+                            color: Colors.grey,
+                          ),
+                          Text(
+                            '찾는중',
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      ))
+                  : ElevatedButton(
+                      onPressed: () async {
+                        await puck.scan();
+
+                        setState(() {
+                          isFinding = true;
+                        });
+                      },
+                      child: Text('찾기'),
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: Size(9999, 40),
+                          backgroundColor: Colors.black),
+                    ),
+              SizedBox(
+                height: 20,
+              ),
+              Heading(title: '등록된 기기'),
+              Heading(title: '연결 가능한 기기'),
+              SizedBox(
+                  height: 300,
+                  child: ListView.builder(
+                      itemCount: puck.scanList.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                            leading: Icon(Icons.device_hub),
+                            title: Text(puck.scanList[index].name),
+                            subtitle: Column(children: <Widget>[
+                              Text(puck.scanList[index].name),
+                            ]));
+                      }))
+            ]),
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  String title = '제목';
+  Heading({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 9999,
+      decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black, width: 1))),
+      child: Text(
+        title,
+        style: TextStyle(
+            color: Colors.grey[600], fontSize: 13, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
